398d9592e39e45ec0324e894e0ea759653db82a7
gadah_almuaikel <gaalmuaikel@stu.kau.edu.sa>
2024-02-27 16:44:35 +0300

    working on adding multiple rows

---
+++ database/migrations/2024_02_22_090733_create_students_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id(); // Creates an auto-incrementing `id` column (unsigned big integer) and makes it the primary key
+            $table->string('name'); // Example: student name
+            $table->string('email')->unique(); // Example: student email
+            $table->date('dob'); // Example: date of birth
+            $table->timestamps(); // Creates `created_at` and `updated_at` columns
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
